a97d0af3632cb5d4421ebf977c7b7e53daf65452
tirthas970-cmyk <tirthas970@gmail.com>
2025-12-24 18:08:01 -0500

    Add files via upload

---
+++ ChatbotTesting.py
@@ -0,0 +1,49 @@
+#Chat Bot test
+
+import json
+import requests;
+
+def FindDefinition(): 
+    #sentence for now should have no puncutation and there should not be any uppercase letters
+    #testing
+
+    sentence = input("Input sentence: ")
+
+    def_list = [] #word list
+    words = sentence.split()
+
+    for word in words:
+        word.lower().strip(".,!,?")
+        
+
+        url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}" #API URL
+
+        response = requests.get(url) #actually gets the info 
+
+        data  = response.json() #json gets data into string form 
+
+        #data is nested, so we have to get first info:
+
+       # data[0] → The first entry found.
+       #['meanings'][0] → The first part of speech (like "noun" or "verb").
+       #['definitions'][0] → The first definition block.
+       #['definition'] → The actual sentence defining the word.
+
+
+        try:
+            definition = data[0]['meanings'][0]['definitions'][0]['definition']
+        except KeyError as e:
+            print(f"KeyError: {e}. Check the structure of your 'data' variable.")
+        except IndexError as e:
+            print(f"IndexError: {e}. A list index is out of range.")
+
+        def_list.append(f'{word}: {definition}')
+
+    #just a quick little display to make things look neat
+    for index, word_def in enumerate(def_list):
+        print(f"""
+{index + 1}. {word_def}
+              """)
+
+
+FindDefinition()
